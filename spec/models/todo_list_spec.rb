cd365dbfd4f8588614b9e1c64877630074ce392a
Seth Reece <sethorama@bassplayer.st>
2014-11-06 14:26:56 -0500

    End of project

--- spec/models/todo_list_spec.rb
+++ spec/models/todo_list_spec.rb
@@ -2,4 +2,32 @@ require 'rails_helper'
 
 RSpec.describe TodoList, :type => :model do
   it { should have_many(:todo_items) }
+
+  describe "#has_completed_items?" do
+  	let(:todo_list) { TodoList.create(title: "Groceries", description: "Grocery List") }
+
+  	it "returns true with completed todo list items" do
+  		todo_list.todo_items.create(content: "Eggs", completed_at: 1.minute.ago)
+  		expect(todo_list.has_completed_items?).to be true
+  	end
+
+  	it "returns false with no completed todo list items" do
+  		todo_list.todo_items.create(content: "Eggs")
+  		expect(todo_list.has_completed_items?).to be false
+  	end
+  end
+
+  describe "#has_incomplete_items?" do
+  	let(:todo_list) { TodoList.create(title: "Groceries", description: "Grocery List") }
+
+  	it "returns true with incompleted todo list items" do
+  		todo_list.todo_items.create(content: "Eggs")
+  		expect(todo_list.has_incomplete_items?).to be true
+  	end
+
+  	it "returns false with no incompleted todo list items" do
+  		todo_list.todo_items.create(content: "Eggs", completed_at: 1.minute.ago)
+  		expect(todo_list.has_incomplete_items?).to be false
+  	end
+  end
 end
